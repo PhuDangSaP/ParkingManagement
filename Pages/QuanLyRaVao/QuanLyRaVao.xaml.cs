84b55bdf8c09bb46d44ac2547579e1d1e2f9ab56
PhuDangSaP <21522464@gm.uit.edu.vn>
2024-12-07 20:33:55 +0700

    Revert "Quản lý ra vào"
    This reverts commit 67471a404e046b0fbbfc5b265fa1f6a51823956b.

--- Pages/QuanLyRaVao/QuanLyRaVao.xaml.cs
+++ Pages/QuanLyRaVao/QuanLyRaVao.xaml.cs
@@ -1,7 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Driver;
-using ParkingManagement.Pages.QuanLyRaVao;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,69 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ParkingManagement.Pages.QuanLyRaVao
+namespace ParkingManagement.Pages
 {
     /// <summary>
     /// Interaction logic for QuanLyRaVao.xaml
     /// </summary>
     public partial class QuanLyRaVao : Page
     {
-
-        private List<ChiTiet> listChiTietRaVao = new List<ChiTiet>();
-
         public QuanLyRaVao()
         {
             InitializeComponent();
-            GetListChiTietRaVao();
-            ListChiTiet.ItemsSource = listChiTietRaVao;
-        }
-
-        private void GetListChiTietRaVao()
-        {
-            listChiTietRaVao.Clear();
-            List<BsonDocument> list = DatabaseHandler.Instance.GetCollection("ChiTietRaVao").Find(new BsonDocument()).ToList();
-            foreach (BsonDocument item in list)
-            {
-                string maCTRaVao = item["MaCTRaVao"].AsString;
-                string maKH = item["MaKH"].AsString;
-                DateTime thoiGianVao = item["ThoiGianVao"].ToUniversalTime();
-                DateTime? thoiGianRa = item.Contains("ThoiGianRa") ? item["ThoiGianRa"].ToNullableUniversalTime() : null;
-
-                listChiTietRaVao.Add(new ChiTiet
-                {
-                    MaCTRaVao = maCTRaVao,
-                    MaKH = maKH,
-                    ThoiGianVao = thoiGianVao,
-                    ThoiGianRa = thoiGianRa
-                });
-            }
-            ListChiTiet.Items.Refresh();
-        }
-
-        private void AddChiTiet_Click(object sender, RoutedEventArgs e)
-        {
-            AddChiTiet dialog = new AddChiTiet();
-            dialog.ShowDialog();
-            GetListChiTietRaVao();
-        }
-
-        private void EditChiTiet_Click(object sender, RoutedEventArgs e)
-        {
-            Button btn = sender as Button;
-            var filter = Builders<BsonDocument>.Filter.Eq("MaCTRaVao", btn.Tag);
-            BsonDocument result = DatabaseHandler.Instance.GetCollection("ChiTietRaVao").Find(filter).FirstOrDefault();
-
-            ChiTiet chiTiet = new ChiTiet
-            {
-                MaCTRaVao = result["MaCTRaVao"].AsString,
-                MaKH = result["MaKH"].AsString,
-                ThoiGianVao = result["ThoiGianVao"].ToUniversalTime(),
-                ThoiGianRa = result.Contains("ThoiGianRa") ? result["ThoiGianRa"].ToNullableUniversalTime() : null
-            };
-
-            EditChiTiet dialog = new EditChiTiet(chiTiet);
-            dialog.ShowDialog();
-            GetListChiTietRaVao();
         }
     }
 }
