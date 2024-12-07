1f208fec439a4515293b6f490184d8502b19b12e
hwthang <22521325@gm.uit.edu.vn>
2024-12-08 01:27:27 +0700

    update qlkh

--- Pages/QuanLyKhachHang/QuanLyKhachHang.xaml.cs
+++ Pages/QuanLyKhachHang/QuanLyKhachHang.xaml.cs
@@ -3,6 +3,7 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -25,109 +26,56 @@ namespace ParkingManagement.Pages.QuanLyKhachHang
     {
         private List<KhachHang>  DsKH = new List<KhachHang>();
 
+
         public QuanLyKhachHang()
         {
             InitializeComponent();
             LayDsKH();
             
-            dtDsKH.ItemsSource = DsKH;
+          
      
         }
         
         private bool kiemTraKH(KhachHang khach)
         {
             if (khach.TenKH == "Nhập họ tên khách hàng" || Regex.IsMatch(khach.TenKH, @"^[\d\s]+$")) return false;
+            if (khach.Cccd == "Nhập số CCCD" || Regex.IsMatch(khach.Cccd, @"^[a-zA-Z]+$")) return false;
+            if (khach.Sdt == "Nhập số điện thoại" ||Regex.IsMatch(khach.Sdt, @"^[a-zA-Z]+$")) return false;
+
             return true;
         }
         private void LayDsKH()
         {
             DsKH.Clear();
-            //KhachHang Kh1 = new KhachHang();
-            //KhachHang Kh2 = new KhachHang();
-            //KhachHang Kh3 = new KhachHang();
-            //KhachHang Kh4 = new KhachHang();
 
-            //Kh1.taoKH("001", "Dang Huu Thang", "000000000000", "Nam", "0933386019", "19/2 Tran Quang Khai", "A304");
-            //DsKH.Add(Kh1);
 
-            //Kh2.taoKH("002", "Nguyen Thi Thanh Hang", "000000000000", "Nu", "0933386019", "19/2 Tran Quang Khai", "A304");
-            //DsKH.Add(Kh2);
 
-            //Kh3.taoKH("003", "Trinh Thi Thu", "000000000000", "Nu", "0933386019", "19/2 Tran Quang Khai", "A304");
-            //DsKH.Add(Kh3);
-
-            //Kh4.taoKH("004", "Nguyen Van Dong", "000000000000", "Nam", "0933386019", "19/2 Tran Quang Khai", "A304");
-            //DsKH.Add(Kh4);
-
-            //listBaiDoOto.Clear();
-            //var filter = Builders<BsonDocument>.Filter.Eq("M", "Oto");
             List<BsonDocument> list = DatabaseHandler.Instance.GetCollection("KhachHang").Find(new BsonDocument()).ToList();
+
+
             foreach (BsonDocument item in list)
             {
-                string maKH = item["MaBD"].AsString;
-                string tenKH = item["TenBD"].AsString;
-                string cccd = item["LoaiBD"].AsString;
+                string maKH = item["MaKH"].AsString;
+                string tenKH = item["TenKH"].AsString;
+                string cccd = item["CCCD"].AsString;
                 string gioitinh = item["GioiTinh"].AsString;
                 string diachi = item["DiaChi"].AsString;
                 string sdt = item["SDT"].AsString;
                 string maxe = item["MaXe"].AsString;
-               
+
 
                 DsKH.Add(new KhachHang { MaKH = maKH, TenKH = tenKH, Cccd = cccd, GioiTinh = gioitinh, DiaChi = diachi, MaXe = maxe, Sdt = sdt });
             }
 
+            dtDsKH.ItemsSource = null;
+            dtDsKH.ItemsSource = DsKH;
 
         }
-
-        private int themKH()
-        {
-
-            KhachHang khach = new KhachHang();
-
-            string id = (DsKH.Count + 1).ToString();
-
-
-            khach.taoKH(id, tenKH.Text, cccd.Text, gt.Text, sdt.Text, dc.Text, maXe.Text);
-
-            if (kiemTraKH(khach))
-            {
-                DsKH.Add(khach);
-
-                dtDsKH.ItemsSource = null;
-                dtDsKH.ItemsSource = DsKH;
-
-                return 1;
-            }
-            return 0;
-            
-
-
-        }
+       
         
-
-    
-
-        private void TimKh_Click(object sender, RoutedEventArgs e)
-        {
-            string search = SearchBox.Text;
-            if (!(search == string.Empty || search == "Nhập mã khách hàng cần tìm")) {
-                List<KhachHang> searchs = new List<KhachHang>();
-                searchs = DsKH.FindAll(item => item.MaKH.Contains(search));
-
-                dtDsKH.ItemsSource = searchs;
-            }
-        
-        }
-
-        private void ThemKH_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            ThemKhachHang.Visibility = Visibility.Visible;
-            panel.Visibility = Visibility.Visible;
-        }
-
         private void Thoat_Click(object sender, RoutedEventArgs e)
         {
-            ThemKhachHang.Visibility = Visibility.Hidden;
+            formKH.Visibility = Visibility.Hidden;
             panel.Visibility = Visibility.Hidden;
         }
 
@@ -198,7 +146,7 @@ namespace ParkingManagement.Pages.QuanLyKhachHang
             if (dc.Text == string.Empty)
             {
                 dc.Text = "Nhập địa chỉ khách hàng";
-                dc.FontWeight = FontWeights.Thin;
+                
 
             }
         }
@@ -207,7 +155,7 @@ namespace ParkingManagement.Pages.QuanLyKhachHang
         {
            if(sdt.Text == "Nhập số điện thoại")
                 sdt.Text = string.Empty;
-            sdt.FontWeight = FontWeights.Regular;
+            
         }
 
         private void sdt_LostFocus(object sender, RoutedEventArgs e)
@@ -215,21 +163,83 @@ namespace ParkingManagement.Pages.QuanLyKhachHang
             if (sdt.Text == string.Empty)
             {
                 sdt.Text = "Nhập số điện thoại";
-                sdt.FontWeight = FontWeights.Thin;
+             
 
             }
         }
 
-        private void LuuKH_Click(object sender, RoutedEventArgs e)
+        private async void LuuKH_Click(object sender, RoutedEventArgs e)
         {
-            if (themKH() == 0) MessageBox.Show("Xử lý không thành công");
+
+            if (tieuDe.Text == "NHẬP THÔNG TIN KHÁCH HÀNG")
+            {
+                var khach = new KhachHang();
+
+                string id = (DsKH.Count + 1).ToString();
+                khach.taoKH(id, tenKH.Text, cccd.Text, gt.Text, sdt.Text, dc.Text, maXe.Text);
+                if (kiemTraKH(khach))
+                {
+                    DsKH.Add(khach);
+
+
+                    await DatabaseHandler.Instance.GetCollection("KhachHang").InsertOneAsync(new BsonDocument {
+                {"MaKH", id } ,
+                { "TenKH", tenKH.Text },
+                {"CCCD", cccd.Text },
+                  {"SDT", sdt.Text },
+                    {"GioiTinh", gt.Text },
+                      {"DiaChi", dc.Text },
+                {"MaXe", maXe.Text } });
+
+                    LayDsKH();
+                    MessageBox.Show("Thêm khách hàng thành công");
+                    Thoat_Click(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("Thông tin khách hàng không hợp lệ");
+                }
+             
+            }
+
             else
             {
-                MessageBox.Show("Thêm thành công");
-                Thoat_Click(sender, e);
+                var khach = (KhachHang)dtDsKH.SelectedItem;
+
+                if (kiemTraKH(khach)) {
+                    // Create the filter to find the document to delete
+                    var filter = Builders<BsonDocument>.Filter.Eq("MaKH", khach.MaKH);
+
+                    // Retrieve the collection
+                    var _collection = DatabaseHandler.Instance.GetCollection("KhachHang");
+
+                    // Perform the delete operation
+                    var result = await _collection.ReplaceOneAsync(filter, new BsonDocument
+                { {"MaKH", khach.MaKH } ,
+                { "TenKH", tenKH.Text },
+                {"CCCD", cccd.Text },
+                  {"SDT", sdt.Text },
+                    {"GioiTinh", gt.Text },
+                      {"DiaChi", dc.Text },
+                {"MaXe", maXe.Text } });
+
+                    LayDsKH();
+                    MessageBox.Show("Cập nhật thành công");
+                    Thoat_Click(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("Thông tin khách hàng không hợp lệ");
+                }
+               
             }
-           
+        
+                  
+
+            
+
         }
+        
 
         private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
         {
@@ -243,19 +253,111 @@ namespace ParkingManagement.Pages.QuanLyKhachHang
             if (SearchBox.Text == string.Empty)
             {
                 SearchBox.Text = "Nhập mã khách hàng cần tìm";
-                SearchBox.FontWeight = FontWeights.Thin;
+                
 
             }
         }
 
-        private void XoaKH_MouseDown(object sender, MouseButtonEventArgs e)
+     
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            var selectedRow = dtDsKH.SelectedItem as KhachHang;
+            string search = SearchBox.Text;
+            if(!(string.IsNullOrEmpty(search)||search == "Nhập mã khách hàng cần tìm")) { 
+                List<KhachHang> searchs = DsKH.FindAll(item => item.MaKH.Contains(SearchBox.Text));
 
-            DsKH.Remove(selectedRow);
+                dtDsKH.ItemsSource = searchs;
+            }
 
+
+           
+
+           
+
+
+
+
+
+
+
+        }
+
+        private void btnThemKH_Click(object sender, RoutedEventArgs e)
+        {
+            tieuDe.Text = "NHẬP THÔNG TIN KHÁCH HÀNG";
+            formKH.Visibility = Visibility.Visible;
+            panel.Visibility = Visibility.Visible;
+            tenKH.FontWeight = FontWeights.Regular;
+            cccd.FontWeight = FontWeights.Regular;
+            sdt.FontWeight = FontWeights.Regular;
+            dc.FontWeight = FontWeights.Regular;
+            maXe.FontWeight = FontWeights.Regular;
+            tenKH.Text = "Nhập họ tên khách hàng";
+            cccd.Text = "Nhập số CCCD";
+            sdt.Text = "Nhập số điện thoại";
+            dc.Text = "Nhập địa chỉ khách hàng";
+            maXe.Text = "Nhập mã xe";
+
+
+        }
+
+        private void dtDsKH_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            tieuDe.Text = "THÔNG TIN KHÁCH HÀNG";
+            formKH.Visibility = Visibility.Visible;
+            panel.Visibility = Visibility.Visible;
+
+            var khach = (KhachHang)dtDsKH.SelectedItem;
+
+            tenKH.FontWeight = FontWeights.Regular;
+            cccd.FontWeight = FontWeights.Regular;
+            sdt.FontWeight = FontWeights.Regular;
+            dc.FontWeight = FontWeights.Regular;
+            maXe.FontWeight = FontWeights.Regular;
+            tenKH.Text = khach.TenKH;
+            cccd.Text = khach.Cccd;
+            sdt.Text = khach.Sdt;
+            dc.Text = khach.DiaChi;
+            maXe.Text = khach.MaXe;
+            gt.Text = khach.GioiTinh;
+        }
+
+        private async void btnXoaKH_Click(object sender, RoutedEventArgs e)
+        {
+            var khach = (KhachHang)dtDsKH.SelectedItem; // Ensure dtDsKH.SelectedItem is not null before casting
+            DsKH.Remove(khach);
             dtDsKH.ItemsSource = null;
             dtDsKH.ItemsSource = DsKH;
+            if (khach != null)
+            {
+                // Create the filter to find the document to delete
+                var filter = Builders<BsonDocument>.Filter.Eq("MaKH", khach.MaKH);
+
+                // Retrieve the collection
+                var _collection = DatabaseHandler.Instance.GetCollection("KhachHang");
+
+                // Perform the delete operation
+                var result = await _collection.DeleteOneAsync(filter);
+
+                // Optional: Check if the deletion was successful
+                if (result.DeletedCount > 0)
+                {
+                    DsKH.Remove(khach);
+                    dtDsKH.ItemsSource = null;
+                    dtDsKH.ItemsSource = DsKH;
+                    MessageBox.Show("Xóa thành công");
+                }
+                else
+                {
+                    MessageBox.Show("Xử lý thất bại");
+                }
+            
+            }
+        }
+
+        private void reload_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            LayDsKH();
         }
     }
 
