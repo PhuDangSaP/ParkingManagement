dabc1810fe81e180a9904657f2e8703e9d39d246
hauyuki <21520835@gm.uit.edu.vn>
2024-12-08 11:19:51 +0700

    update Quanlyve

---
+++ Ve.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace ParkingManagement
+{
+    public class Ve
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("MaVe")]
+        public string MaVe { get; set; }
+
+        [BsonElement("MaLoaiVe")]
+        public string MaLoaiVe { get; set; }
+
+        [BsonElement("MaKhachHang")]
+        public string MaKhachHang { get; set; }
+
+        [BsonElement("NgayKichHoat")]
+        public DateTime NgayKichHoat { get; set; }
+
+        [BsonElement("TinhTrang")]
+        public string TinhTrang { get; set; }
+    }
+}
